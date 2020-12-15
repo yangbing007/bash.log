0ade7ffdef1bc28a5edd88b7f8ea9ecb019d3a2f
yangbing007 <yangbinggoooooogle@gmail.com>
2020-12-15 20:39:48 +0800

    ...

---
+++ delete-npm-proxy.bat
@@ -0,0 +1,8 @@
+###npm config set proxy http://username:password@server:port
+###npm confit set https-proxy http://username:password@server:port
+
+npm config delete proxy
+npm config delete https-proxy
+
+npm config get proxy
+npm config get https-proxy
