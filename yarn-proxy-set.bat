c39386df2c467a55db7884cd2485645809112b11
yangbing007 <yangbinggoooooogle@gmail.com>
2020-12-15 20:49:18 +0800

    ...

---
+++ yarn-proxy-set.bat
@@ -0,0 +1,8 @@
+yarn config set proxy http://username:password@server:port
+yarn confit set https-proxy http://username:password@server:port
+
+###yarn config delete proxy --global
+###yarn config delete https-proxy --global
+
+yarn config get proxy
+yarn config get https-proxy
